cd1064ef80a6671411f703f4d281f85481f555d2
Richard <richokoye1@gmail.com>
2021-11-22 16:08:32 +0100

    add task model

---
+++ src/models/task.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema( {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false,
+        required: false
+    }
+});
+
+
+const Task = mongoose.model('Task', taskSchema)
+
+module.exports = Task;
